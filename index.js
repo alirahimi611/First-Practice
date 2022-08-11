b2ea2d610aae64635910dfb3daa65554c8f100b0
Ali.r <alirahimi611@gmail.com>
2022-08-11 09:27:48 +0430

    edit on login

--- index.js
+++ index.js
@@ -14,10 +14,3 @@ console.log("ali");
 console.log("ali");
 console.log("ali");
 
-const ali = (value)=>{
-    console.log(value);
-}
-
-
-ali("ali")
->>>>>>> ac6cd1b24113557b009d3ca83ce6435fb50266b5
